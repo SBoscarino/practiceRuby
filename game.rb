5f6c6a1cc097b02410585a613baaf4660161ede6
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2017-09-21 18:30:23 -0500

    adding players and health

--- game.rb
+++ game.rb
@@ -1,10 +1,14 @@
-greeting = "welcome!"
+greeting = "Welcome!"
 puts greeting
-puts "It is", Time.new
 
-stefHealth = 100
-bobHealth = 100
+player1 = "Crystal"
+player2 = "Sid"
+player3 = "Thorus"
+
+crystalHealth = 100
 sidHealth = 100
 thorusHealth = 100
 
-puts "Stef's Health is #{stefHealth}" 
+puts "#{player1}'s health is #{crystalHealth}"
+puts "#{player2}'s health is #{sidHealth / 1.2}"
+puts "#{player3}' health is #{thorusHealth * 3}"
